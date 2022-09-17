4e4c5e7a27b9acc8e7e0a1c928b906e336e000e8
kashpovski <kashpovski@gmail.com>
2022-09-17 17:33:02 +0300

    logger listener

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import logging
 import datetime
 
 from selenium import webdriver
+from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
 
 
 def pytest_addoption(parser):
@@ -23,14 +24,80 @@ def browser(request):
     fullscreen = request.config.getoption("--fullscreen")
     log_level = request.config.getoption("--log_level")
 
-    logger = logging.getLogger(request.node.name)
-    file_handler = logging.FileHandler(f"logs/{request.function.__name__}.log")
-    file_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s %(funcName)s [%(module)s] | %(levelname)s :  %(message)s"))
-    logger.addHandler(file_handler)
-    logger.setLevel(level=log_level)
+    class MyLogger(AbstractEventListener):
+        logger = logging.getLogger(request.node.name)
+        file_handler = logging.FileHandler(f"logs/{request.function.__name__}.log")
+        file_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s %(funcName)s [%(module)s] "
+                                                    "| %(levelname)s :  %(message)s"))
+        logger.addHandler(file_handler)
+        logger.setLevel(level=log_level)
 
-    start_time = datetime.datetime.now()
-    logger.info(f"===> Test started ===>")
+        def before_navigate_to(self, url, driver):
+            # self.logger.info(f"I'm navigating to {url} and {driver.title}")
+            pass
+
+        def after_navigate_to(self, url, driver):
+            self.logger.info(f"I'm on {url} and {driver.title}")
+
+        def before_navigate_back(self, driver):
+            # self.logger.info(f"I'm navigating back")
+            pass
+
+        def after_navigate_back(self, driver):
+            self.logger.info(f"I'm back!")
+
+        def before_navigate_forward(self, driver):
+            # self.logger.info(f"{driver}")
+            pass
+
+        def after_navigate_forward(self, driver):
+            self.logger.info(f"{driver}")
+
+        def before_find(self, by, value, driver):
+            # self.logger.info(f"I'm looking for '{value}' with '{by}'")
+            pass
+
+        def after_find(self, by, value, driver):
+            self.logger.info(f"I've found '{value}' with '{by}'")
+
+        def before_click(self, element, driver):
+            # self.logger.info(f"I'm clicking {element}")
+            pass
+
+        def after_click(self, element, driver):
+            self.logger.info(f"I've clicked {element}")
+
+        def before_change_value_of(self, element, driver):
+            # self.logger.info(f"{element}, {driver}")
+            pass
+
+        def after_change_value_of(self, element, driver):
+            self.logger.info(f"{element}, {driver}")
+
+        def before_execute_script(self, script, driver):
+            # self.logger.info(f"I'm executing '{script}'")
+            pass
+
+        def after_execute_script(self, script, driver):
+            self.logger.info(f"I've executed '{script}'")
+
+        def before_close(self, driver):
+            self.logger.info(f"{driver}")
+
+        def after_close(self, driver):
+            self.logger.info(f"{driver}")
+
+        def before_quit(self, driver):
+            self.logger.info(f"I'm getting ready to terminate {driver}")
+
+        def after_quit(self, driver):
+            self.logger.info(f"Driver Quit")
+
+        def on_exception(self, exception, driver):
+            pass
+
+    # start_time = datetime.datetime.now()
+    # logger.info(f"===> Test started ===>")
 
     options = webdriver.ChromeOptions()
     if headless:
@@ -55,23 +122,16 @@ def browser(request):
         _browser.maximize_window()
 
     _browser.url = url
-    _browser.test_file = request.function.__name__
+    _browser = EventFiringWebDriver(_browser, MyLogger())
     _browser.test_name = request.node.name
     _browser.log_level = log_level
-    _browser.logger = logger
 
-    logger.info(f"Browser: {browser_name} ({_browser.session_id})")
+    # logger.info(f"Browser: {browser_name} ({_browser.session_id})")
 
     def fin():
         _browser.quit()
-        logger.info(f"<=== Test finished. {datetime.datetime.now() - start_time} <===")
+        # logger.info(f"<=== Test finished. {datetime.datetime.now() - start_time} <===")
 
     request.addfinalizer(fin)
 
     return _browser
-
-    # yield _browser
-    #
-    # logger.info(f"===/// Test finished. {datetime.datetime.now() - start_time}")
-    #
-    # _browser.quit()
